92a03698ecbe27aabfd334b170052aaf0dc47ceb
Fajri135 <corex135@gmail.com>
2024-03-28 11:26:15 +0700

    Memanggil variabel kiri sub array untuk diurutkan

--- QuickSort_0189.cpp
+++ QuickSort_0189.cpp
@@ -67,6 +67,11 @@ void q_sort(int low, int high) {
         swap(low, j);
     }
 
+    // Recursive call to sort the left sub array
+    q_sort(low, j - 1); // Step 12
+
+
+
     int main(); {
         ;
     }
